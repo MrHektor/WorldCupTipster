16bb7938bbe1a0ff6b06743fc6bfc3325284c96f
Brandlarm <marcus.hektor@gmail.com>
2018-03-07 12:19:15 +0100

    Fixed axios post

--- wwwroot/js/login.js
+++ wwwroot/js/login.js
@@ -5,28 +5,40 @@ var loginApp = new Vue({
         password: '',
         loaded: false
     },
-    mounted: function(){
+    mounted: function () {
         this.loaded = true;
     },
     methods: {
-        login: function(){
-            // Call api to check login information            
+        login: function () {
+            // Call api to check login information
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+            let fd = new FormData();
+            fd.append('email', this.email);
+            fd.append('password', this.password);
+            return axios.post("http://localhost:81/api/tipster/LoginUser", fd, config)
+                .then(function (response) {
+                    alert("inloggad");
 
-            // Temp test 
-            if(this.email === "test" && this.password === "test"){
-                var user = {
-                    id: 1,
-                    email: 'enepost@gmail.com',
-                    firstname: 'Kalle',
-                    lastname: 'Anka'
-                };
+                    // localStorage.setItem("user",JSON.stringify(user));                
+                    // app.checkLogin();
+                })
+                .catch(function (error) {
+                    alert(error);
+                });
+            // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+            // axios.post('http://localhost:81/api/tipster/LoginUser', {
+            //     email: 'hej@hopp.nu',
+            //     password: 'test'
+            //   }, config)
+            //   .then(function (response) {
+            //     alert("inloggad");
 
-                localStorage.setItem("user",JSON.stringify(user));                
-                app.checkLogin();
-            }
-            else{
-                alert("Inloggning misslyckades");
-            }
-        }        
-    }           
+            //     // localStorage.setItem("user",JSON.stringify(user));                
+            //     // app.checkLogin();
+            //   })
+            //   .catch(function (error) {
+            //     alert(error);
+            //   });
+        }
+    }
 });
